e06b85fa4ddf8f51f7e75d869a3f867837133d76
Visweshvar-UST <287618@ust.com>
2024-10-01 12:19:40 +0530

    dummy

--- src/main/java/com/demo/spring_rabbitmq_consumer/MQConfig.java
+++ src/main/java/com/demo/spring_rabbitmq_consumer/MQConfig.java
@@ -12,6 +12,7 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class MQConfig {
     public static final String QUEUE = "message_queue";
